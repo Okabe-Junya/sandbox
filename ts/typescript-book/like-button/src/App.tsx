a2907b8f20b23074872289879aa2a0e358bdc970
Junya Okabe <okabe.junya.qj@alumni.tsukuba.ac.jp>
2023-05-23 02:10:27 +0900

    add: ボタンの雛形を作る

--- ts/typescript-book/like-button/src/App.tsx
+++ ts/typescript-book/like-button/src/App.tsx
@@ -6,21 +6,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello TypeScript! <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LikeButton />
       </header>
     </div>
   );
+}
+
+function LikeButton() {
+  return <span>TODO: ここに何か書く</span>
 }
 
 export default App;
